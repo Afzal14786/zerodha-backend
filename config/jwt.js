11884510692f32df4f42c85967eecccc78d13973
Afzal14786 <mdafzal14777@gmail.com>
2025-08-27 22:54:25 +0530

    adding routes and etup the redisClint and jwt.js

---
+++ config/jwt.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+export const generateAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+export const generateRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+  });
+};
+
+export const verifyToken = (token, secret = process.env.JWT_SECRET) => {
+  try {
+    return jwt.verify(token, secret);
+  } catch (err) {
+    return null;
+  }
+};
---
+++ routers/user/register.router.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { wrapError } from "../../middleware/wrapError.js";
+
+import {
+  phoneSignUp,
+  verifyPhoneOtp,
+  saveLeadAndVerifyOtp,
+  setPasswordAndCreateAccount,
+} from "../../controller/user/register.user.js";
+
+const router = express.Router();
+
+router.post("/signup", wrapError(phoneSignUp));
+router.post("/open-account", wrapError(phoneSignUp));
+router.post("/verify-mobile", wrapError(verifyPhoneOtp));
+router.post("/lead-info", wrapError(saveLeadAndVerifyOtp));
+router.post("/set-password", wrapError(setPasswordAndCreateAccount));
+
+export default router;
